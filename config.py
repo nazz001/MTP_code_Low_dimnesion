45bfd4311e452a749de5d478b6497fb792c8cda8
N P <nishkalprakash@gmail.com>
2026-01-28 15:52:47 +0530

    here is my dataset

---
+++ config.py
@@ -0,0 +1,16 @@
+# datapath="/home/nishkal/datasets/iris_db"
+casia_iris_thousand="/home/nishkal/datasets/iris_db/CASIA_iris_thousand/worldcoin_outputs_images"
+# irig contain orig image,
+casia_iris_thousand_normalized="/home/nishkal/datasets/iris_db/CASIA_iris_thousand/worldcoin_outputs_images/normalized"
+# it contains normalized data 
+# ----------------------------------------casia iris thousand ---------------------------
+casia_v1="/home/nishkal/datasets/iris_db/CASIA_v1"
+casia_v1_normalized="/home/nishkal/datasets/iris_db/CASIA_v1/worldcoin_outputs_images/normalized"
+
+# ---------------------------------casia  v1 -----------------------------------
+iitd_v1="/home/nishkal/datasets/iris_db/IITD_v1"
+iitd_v1_normalized="/home/nishkal/datasets/iris_db/IITD_v1/worldcoin_outputs_images/normalized"
+
+
+# --------------------------------------------iitd v1 ----------------------------------
+
